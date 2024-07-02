0ade433136ebf074b93c472238967c29937536fa
Yurii <fanfjrfj589@gmail.com>
2024-07-02 19:37:12 +0200

    fix bugs

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
-
 export const GET = async (req: any, { params }: { params: { slug: string } }) => {
   const { slug } = params;
 
