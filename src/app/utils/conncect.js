b3a221eb265d4438a5117199e89c3b200458b637
Yurii <fanfjrfj589@gmail.com>
2024-07-03 07:51:07 +0200

    try to fix

--- src/app/utils/conncect.js
+++ src/app/utils/conncect.js
@@ -3,13 +3,13 @@
 import { PrismaClient } from '@prisma/client';
 
 if (process.env.NODE_ENV === 'production') {
-  global.prisma = new PrismaClient();
+  global.prisma = global.prisma || new PrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
 }
 
-const prisma = global.prisma as PrismaClient;
+const prisma = global.prisma;
 
 export default prisma;
